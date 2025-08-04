9193a23cff5b3741de6508a63a6d1f27f875f0c7
Himanish Ganjoo <101480126+hganjoo@users.noreply.github.com>
2025-08-04 14:13:26 +0200

    Added forcep allocation to init_part
    No 3 on phi_psi list

--- src/pm/init_part.f90
+++ src/pm/init_part.f90
@@ -65,6 +65,12 @@ subroutine init_part
   ! Allocate particle variables
   allocate(xp    (npartmax,ndim))
   allocate(vp    (npartmax,ndim))
+
+  ! Allocate forces for extradof
+  #ifdef OUTPUT_EXTRADOF_PART
+  allocate (forcep(npartmax, 3*ndim + 3))
+  forcep = 0.0
+  #endif
   allocate(mp    (npartmax))
   allocate(nextp (npartmax))
   allocate(prevp (npartmax))
