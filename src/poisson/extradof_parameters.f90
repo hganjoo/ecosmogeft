9cd34c9efcbfda169b9f5ba491c699f989f35f1a
Himanish Ganjoo <101480126+hganjoo@users.noreply.github.com>
2025-09-12 10:38:40 +0200

    C2 definition changed back to without w0-wa

--- src/poisson/extradof_parameters.f90
+++ src/poisson/extradof_parameters.f90
@@ -13,9 +13,9 @@ module extradof_parameters
   ! speed of light [meter/sec]
   real(dp) :: sol=299792458.0d0
 
-  integer  :: ngs_fine_extradof_pre   = 36  ! pre-smoothing factor
-  integer  :: ngs_fine_extradof_pst   = 3   ! post-smoothing factor
-  integer  :: ngs_coarse_extradof_pre = 36  ! pre-smoothing factor
-  integer  :: ngs_coarse_extradof_pst = 3   ! post-smoothing factor
+  integer  :: ngs_fine_extradof_pre   = 5  ! pre-smoothing factor
+  integer  :: ngs_fine_extradof_pst   = 5   ! post-smoothing factor
+  integer  :: ngs_coarse_extradof_pre = 5  ! pre-smoothing factor
+  integer  :: ngs_coarse_extradof_pst = 5   ! post-smoothing factor
 
 end module extradof_parameters
